457d580e82d3c1d5c7c8ca3ea273917ed9e8f3ef
Annavaramgit <satyanarayanaraavi@123@gmail.com>
2024-10-09 15:53:45 +0530

    initial commit

---
+++ camunda8-connectors-tasks/src/main/java/com/camunda8/connectors/Camunda8ConnectorsApplication.java
@@ -0,0 +1,13 @@
+package com.camunda8.connectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Camunda8ConnectorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Camunda8ConnectorsApplication.class, args);
+	}
+
+}
